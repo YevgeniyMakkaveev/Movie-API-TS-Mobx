89efd85257957ced8d9ef880488440b3edbbe366
YevgeniyMakkaveev <yevgenmakkav@gmail.com>
2021-11-08 17:47:04 +0300

    Active style fixes

--- src/components/Header/NavButtons/NavButtons.tsx
+++ src/components/Header/NavButtons/NavButtons.tsx
@@ -2,68 +2,64 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavButtons.scss";
 
-const NavButtons: React.FC = () => (
-  <div className="btn_wrap">
-    <div className="btns">
-      <div className="text_btn">
-        <NavLink
-          style={{
-            display: "block",
-            color: "#264653",
-            textDecoration: "none",
-            height: "auto",
-            width: "auto",
-            paddingTop: "0.4em",
-          }}
-          activeStyle={{
-            fontWeight: "bold",
-            color: "#F4F5FF",
-          }}
-          to="/popular"
-        >
-          Popular
-        </NavLink>
-      </div>
-      <div className="text_btn">
-        <NavLink
-          style={{
-            display: "block",
-            color: "#264653",
-            textDecoration: "none",
-            height: "auto",
-            width: "auto",
-            paddingTop: "0.4em",
-          }}
-          activeStyle={{
-            fontWeight: "bold",
-            color: "#F4F5FF",
-          }}
-          to="/search"
-        >
-          Search
-        </NavLink>
-      </div>
-      <div className="text_btn">
-        <NavLink
-          style={{
-            display: "block",
-            color: "#264653",
-            textDecoration: "none",
-            height: "auto",
-            width: "auto",
-            paddingTop: "0.4em",
-          }}
-          activeStyle={{
-            fontWeight: "bold",
-            color: "#F4F5FF",
-          }}
-          to="/favorite"
-        >
-          Favorite
-        </NavLink>
+const NavButtons: React.FC = () => {
+  const navStyle = {
+    display: "flex",
+    color: "#264653",
+    textDecoration: "none",
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return (
+    <div className="btn_wrap">
+      <div className="btns">
+        <div className="text_btn">
+          <NavLink
+            style={navStyle}
+            activeStyle={{
+              fontWeight: "bold",
+              backgroundColor: "#2a9d8f",
+              borderRadius: " 0.5em",
+              color: "#F4F5FF",
+            }}
+            to="/popular"
+          >
+            Popular
+          </NavLink>
+        </div>
+        <div className="text_btn">
+          <NavLink
+            style={navStyle}
+            activeStyle={{
+              fontWeight: "bold",
+              backgroundColor: "#2a9d8f",
+              borderRadius: " 0.5em",
+              color: "#F4F5FF",
+            }}
+            to="/search"
+          >
+            Search
+          </NavLink>
+        </div>
+        <div className="text_btn">
+          <NavLink
+            style={navStyle}
+            activeStyle={{
+              fontWeight: "bold",
+              backgroundColor: "#2a9d8f",
+              borderRadius: " 0.5em",
+              color: "#F4F5FF",
+            }}
+            to="/favorite"
+          >
+            Favorite
+          </NavLink>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NavButtons;
